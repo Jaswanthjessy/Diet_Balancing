b9e24b53a95e0a8b864f52e20ad73e3bb73bbd3c
Jaswanth Golaith <133398168+Jaswanthjessy@users.noreply.github.com>
2024-12-10 18:21:55 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -40,7 +40,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:8080/users/login', {
+      const response = await fetch('https://diet-production-e52e.up.railway.app/users/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
